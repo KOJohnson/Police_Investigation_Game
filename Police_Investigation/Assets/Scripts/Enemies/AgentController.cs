1cbed723f28b8366fc904d620b2152b8c853527c
KOJohnson <86432574+KOJohnson@users.noreply.github.com>
2021-11-03 16:48:25 +0000

    File Management, Animation, Enemy AI
    Organised project files, imported animations from Mixamo, wrote small script that controls enemy movement and attacks.
    
    Co-Authored-By: Ketah James Macauley <50329349+Kmacauley95@users.noreply.github.com>

--- Police_Investigation/Assets/Scripts/Enemies/AgentController.cs
+++ Police_Investigation/Assets/Scripts/Enemies/AgentController.cs
@@ -1,15 +1,51 @@
+using System;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(Animator),typeof(NavMeshAgent))]
 public class AgentController : MonoBehaviour
 {
 
-   public Transform Player;
+   public Transform player;
+   [SerializeField]private NavMeshAgent agent;
+   
+   private float _distanceToPlayer;
+   [SerializeField]private float attackRange;
+   [SerializeField]private float speed;
 
-    void Update()
+   public Animator anim;
+
+   private void Awake()
+   {
+       
+   }
+
+   void Update()
+   {
+       _distanceToPlayer = Vector3.Distance(transform.position, player.position);
+       
+       if (_distanceToPlayer > attackRange)
+       {
+           agent.SetDestination(player.position);
+           MoveToPlayer();
+       }
+       else if (_distanceToPlayer <= attackRange)
+       {
+           AttackPlayer();
+       }
+   }
+
+    private void MoveToPlayer()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = Player.position;
+        agent.speed = speed;
+        anim.Play("Zombie Running 1");
+    }
+
+    private void AttackPlayer()
+    {
+        agent.speed = 0f;
+        anim.Play("Zombie Punch 1");
     }
 
 }
